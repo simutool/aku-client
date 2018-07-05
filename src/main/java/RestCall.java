a5c16adea4273ecffd9f1f5c3ba02f1361853793
Ozkorin <hg2t4e@gmail.com>
2018-07-05 19:36:42 +0200

    Code for Neo4J query

--- src/main/java/RestCall.java
+++ src/main/java/RestCall.java
@@ -2,5 +2,25 @@
 public class RestCall {
 	
 	//This will make REST Calls, and return the result
+	
+	public boolean sendFileMetadata(String created, String userURI) {
+		String dc_identifier = UUIDGenerator.getUUID();
+		String uri = "http://uni-bamberg.de/mobi/kbms/simutool/Data/" + dc_identifier;
+		
+		//Construct Json Query for Neo4J
+		String createDataQuery = "CREATE (d:Data { " + 
+		"created: \"" + created + "\", " +
+		"uri: \"" + uri + "\", " +
+		"dc_identifier: \"" + dc_identifier + "\" })";
+		
+		String matchQuery = "MATCH (d:Data), (u:User) " + 
+		"WHERE d.uri = \"" + uri + "\" " + 
+		"and u.uri = \"" + userURI + "\" " +
+		"CREATE (d)-[:dc_contributor]->(u)";
+		
+		String query = createDataQuery + matchQuery;
+		
+		return false;
+	}
 
 }
